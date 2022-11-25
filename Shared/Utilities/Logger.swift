3202fde54de6d5f1d783f913bf6f6a4bfa3c85f9
Lev Poznyakov <levochkaaa@icloud.com>
2022-11-25 11:30:10 +0300

    logger: removed 'cause of not using

--- Shared/Utilities/Logger.swift
+++ Shared/Utilities/Logger.swift
@@ -7,26 +7,13 @@ struct Logger {
     private let logger: os.Logger
     private let label: String
 
-    enum LogLevel {
-        case info, warning, error, critical
-    }
-
     init(label: String) {
         self.logger = os.Logger(subsystem: "BetterTG", category: label)
         self.label = label
     }
 
-    func log(_ message: String, level: LogLevel = .info) {
+    func log(_ message: String) {
         let date = Date.now.formatted(date: .omitted, time: .standard)
-        switch level {
-            case .info:
-                logger.info("[\(date)] [\(label)] [info] \(message)")
-            case .error:
-                logger.error("[\(date)] [\(label)] [error] \(message)")
-            case .critical:
-                logger.critical("[\(date)] [\(label)] [critical] \(message)")
-            case .warning:
-                logger.warning("[\(date)] [\(label)] [warning] \(message)")
-        }
+        logger.info("[\(date)] [\(label)] \(message)")
     }
 }
