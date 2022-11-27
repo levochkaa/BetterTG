a39f6a215c99a9c65e9d03db3df1af6bee5718dd
Lev Poznyakov <levochkaaa@icloud.com>
2022-11-27 17:01:02 +0300

    View+if, will be very important in future

---
+++ Shared/Utilities/Extensions/View+if.swift
@@ -0,0 +1,32 @@
+// View+if.swift
+
+import SwiftUI
+
+extension View {
+    @ViewBuilder func `if`<TrueContent: View, FalseContent: View>(
+        _ condition: Bool,
+        transform: (Self) -> TrueContent,
+        else elseTransform: ((Self) -> FalseContent)? = nil
+    ) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            if let elseTransform {
+                elseTransform(self)
+            } else {
+                self
+            }
+        }
+    }
+
+    @ViewBuilder func `if`<Content: View>(
+        _ condition: Bool,
+        transform: (Self) -> Content
+    ) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+}
