2118ffea3ddce384fb1c501013223c6b869f5cfc
Lev Poznyakov <levochkaaa@icloud.com>
2022-11-28 14:29:38 +0300

    will need readSize in future

---
+++ Shared/Utilities/Extensions/View+ReadSize.swift
@@ -0,0 +1,15 @@
+// View+ReadSize.swift
+
+import SwiftUI
+
+extension View {
+    func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
+        background(
+            GeometryReader { geometryProxy in
+                Color.clear
+                    .preference(key: SizePreferenceKey.self, value: geometryProxy.size)
+            }
+        )
+            .onPreferenceChange(SizePreferenceKey.self, perform: onChange)
+    }
+}
---
+++ Shared/Utilities/SizePreferenceKey.swift
@@ -0,0 +1,11 @@
+// SizePreferenceKey.swift
+
+import Foundation
+
+struct SizePreferenceKey: PreferenceKey {
+    static var defaultValue: CGSize = .zero
+
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+        value = nextValue()
+    }
+}
