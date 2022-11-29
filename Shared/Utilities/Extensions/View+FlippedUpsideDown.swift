c76abf6853b0021fb7f60b39cb3f250b1fc8ffc9
Lev Poznyakov <levochkaaa@icloud.com>
2022-11-29 21:06:18 +0300

    better syntax

--- Shared/Utilities/Extensions/View+FlippedUpsideDown.swift
+++ Shared/Utilities/Extensions/View+FlippedUpsideDown.swift
@@ -2,16 +2,10 @@
 
 import SwiftUI
 
-struct FlippedUpsideDown: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .rotationEffect(.radians(Double.pi))
-            .scaleEffect(x: -1, y: 1, anchor: .center)
-    }
-}
-
 extension View {
     func flippedUpsideDown() -> some View {
-        modifier(FlippedUpsideDown())
+        self
+            .rotationEffect(.radians(Double.pi))
+            .scaleEffect(x: -1, y: 1, anchor: .center)
     }
 }
