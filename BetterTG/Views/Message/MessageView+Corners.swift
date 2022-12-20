288363e74bbc49271d9bb984a76304e0908c5c49
Lev Poznyakov <levochkaaa@icloud.com>
2022-12-20 11:56:44 +0300

    i hope now all possible cases are handled

--- BetterTG/Views/Message/MessageView+Corners.swift
+++ BetterTG/Views/Message/MessageView+Corners.swift
@@ -14,7 +14,9 @@ extension MessageView {
         switch type {
             case .reply:
                 corners.append(contentsOf: [.topLeading, .topTrailing])
-                if (contentWidth != 0 && replyWidth > contentWidth) || (textWidth != 0 && replyWidth > textWidth) {
+                if (contentWidth != 0 && replyWidth > contentWidth) || (textWidth != 0 && replyWidth > textWidth)
+                    || (contentWidth != 0 && contentWidth - replyWidth < 15)
+                    || (textWidth != 0 && textWidth - replyWidth < 15) {
                     corners.append(isOutgoing ? .bottomLeading : .bottomTrailing)
                 }
                 return corners
@@ -24,18 +26,18 @@ extension MessageView {
                 } else if replyWidth == 0 {
                     corners.append(contentsOf: [.topLeading, .topTrailing])
                 }
-                if textWidth != 0 && textWidth < contentWidth {
+                if (textWidth != 0 && textWidth < contentWidth)
+                    || (textWidth == 0 && editWidth != 0 && editWidth < contentWidth) {
                     corners.append(isOutgoing ? .bottomLeading : .bottomTrailing)
                 } else if textWidth == 0 && editWidth == 0 {
                     corners.append(contentsOf: [.bottomLeading, .bottomTrailing])
-                } else if textWidth == 0 && editWidth != 0 && editWidth < contentWidth {
-                    corners.append(isOutgoing ? .bottomLeading : .bottomTrailing)
                 }
                 return corners
             case .text:
-                if contentWidth != 0 && contentWidth < textWidth {
-                    corners.append(isOutgoing ? .topLeading : .topTrailing)
-                } else if replyWidth != 0 && replyWidth < textWidth {
+                if (contentWidth != 0 && contentWidth < textWidth)
+                    || (contentWidth != 0 && contentWidth - textWidth < 15)
+                    || (replyWidth != 0 && replyWidth < textWidth)
+                    || (replyWidth != 0 && replyWidth - textWidth < 15) {
                     corners.append(isOutgoing ? .topLeading : .topTrailing)
                 } else if replyWidth == 0 && contentWidth == 0 {
                     corners.append(contentsOf: [.topLeading, .topTrailing])
@@ -48,7 +50,9 @@ extension MessageView {
                 return corners
             case .edit:
                 corners.append(contentsOf: [.bottomLeading, .bottomTrailing])
-                if (textWidth != 0 && textWidth < editWidth) || (contentWidth != 0 && contentWidth < editWidth) {
+                if (textWidth != 0 && textWidth < editWidth) || (contentWidth != 0 && contentWidth < editWidth)
+                    || (textWidth != 0 && textWidth - editWidth < 15)
+                    || (contentWidth != 0 && contentWidth - editWidth < 15) {
                     corners.append(isOutgoing ? .topLeading : .topTrailing)
                 }
                 return corners
