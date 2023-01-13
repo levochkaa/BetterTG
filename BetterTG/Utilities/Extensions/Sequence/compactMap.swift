c9cac5af3127f4cfd9ffd540bb9907cacc4a4db9
Lev Poznyakov <levochkaaa@icloud.com>
2023-01-13 23:59:05 +0300

    renamed all files for better reading

---
+++ BetterTG/Utilities/Extensions/Sequence/compactMap.swift
@@ -0,0 +1,98 @@
+// compactMap.swift
+
+import Foundation
+
+extension Sequence {
+    /// Transform the sequence into an array of new values using
+    /// an async closure that returns optional values. Only the
+    /// non-`nil` return values will be included in the new array.
+    ///
+    /// The closure calls will be performed in order, by waiting for
+    /// each call to complete before proceeding with the next one. If
+    /// any of the closure calls throw an error, then the iteration
+    /// will be terminated and the error rethrown.
+    ///
+    /// - parameter transform: The transform to run on each element.
+    /// - returns: The transformed values as an array. The order of
+    ///   the transformed values will match the original sequence,
+    ///   except for the values that were transformed into `nil`.
+    /// - throws: Rethrows any error thrown by the passed closure.
+    func asyncCompactMap<T>(
+        _ transform: (Element) async throws -> T?
+    ) async rethrows -> [T] {
+        var values = [T]()
+        
+        for element in self {
+            guard let value = try await transform(element) else {
+                continue
+            }
+            
+            values.append(value)
+        }
+        
+        return values
+    }
+    
+    /// Transform the sequence into an array of new values using
+    /// an async closure that returns optional values. Only the
+    /// non-`nil` return values will be included in the new array.
+    ///
+    /// The closure calls will be performed concurrently, but the call
+    /// to this function won't return until all of the closure calls
+    /// have completed.
+    ///
+    /// - parameter priority: Any specific `TaskPriority` to assign to
+    ///   the async tasks that will perform the closure calls. The
+    ///   default is `nil` (meaning that the system picks a priority).
+    /// - parameter transform: The transform to run on each element.
+    /// - returns: The transformed values as an array. The order of
+    ///   the transformed values will match the original sequence,
+    ///   except for the values that were transformed into `nil`.
+    func concurrentCompactMap<T>(
+        withPriority priority: TaskPriority? = nil,
+        _ transform: @escaping (Element) async -> T?
+    ) async -> [T] {
+        let tasks = map { element in
+            Task(priority: priority) {
+                await transform(element)
+            }
+        }
+        
+        return await tasks.asyncCompactMap { task in
+            await task.value
+        }
+    }
+    
+    /// Transform the sequence into an array of new values using
+    /// an async closure that returns optional values. Only the
+    /// non-`nil` return values will be included in the new array.
+    ///
+    /// The closure calls will be performed concurrently, but the call
+    /// to this function won't return until all of the closure calls
+    /// have completed. If any of the closure calls throw an error,
+    /// then the first error will be rethrown once all closure calls have
+    /// completed.
+    ///
+    /// - parameter priority: Any specific `TaskPriority` to assign to
+    ///   the async tasks that will perform the closure calls. The
+    ///   default is `nil` (meaning that the system picks a priority).
+    /// - parameter transform: The transform to run on each element.
+    /// - returns: The transformed values as an array. The order of
+    ///   the transformed values will match the original sequence,
+    ///   except for the values that were transformed into `nil`.
+    /// - throws: Rethrows any error thrown by the passed closure.
+    func concurrentCompactMap<T>(
+        withPriority priority: TaskPriority? = nil,
+        _ transform: @escaping (Element) async throws -> T?
+    ) async throws -> [T] {
+        let tasks = map { element in
+            Task(priority: priority) {
+                try await transform(element)
+            }
+        }
+        
+        return try await tasks.asyncCompactMap { task in
+            try await task.value
+        }
+    }
+}
---
+++ BetterTG/Utilities/Extensions/Sequence/map.swift
@@ -0,0 +1,88 @@
+// map.swift
+
+import Foundation
+
+extension Sequence {
+    /// Transform the sequence into an array of new values using
+    /// an async closure.
+    ///
+    /// The closure calls will be performed in order, by waiting for
+    /// each call to complete before proceeding with the next one. If
+    /// any of the closure calls throw an error, then the iteration
+    /// will be terminated and the error rethrown.
+    ///
+    /// - parameter transform: The transform to run on each element.
+    /// - returns: The transformed values as an array. The order of
+    ///   the transformed values will match the original sequence.
+    /// - throws: Rethrows any error thrown by the passed closure.
+    func asyncMap<T>(
+        _ transform: (Element) async throws -> T
+    ) async rethrows -> [T] {
+        var values = [T]()
+        
+        for element in self {
+            try await values.append(transform(element))
+        }
+        
+        return values
+    }
+    
+    /// Transform the sequence into an array of new values using
+    /// an async closure.
+    ///
+    /// The closure calls will be performed concurrently, but the call
+    /// to this function won't return until all of the closure calls
+    /// have completed.
+    ///
+    /// - parameter priority: Any specific `TaskPriority` to assign to
+    ///   the async tasks that will perform the closure calls. The
+    ///   default is `nil` (meaning that the system picks a priority).
+    /// - parameter transform: The transform to run on each element.
+    /// - returns: The transformed values as an array. The order of
+    ///   the transformed values will match the original sequence.
+    func concurrentMap<T>(
+        withPriority priority: TaskPriority? = nil,
+        _ transform: @escaping (Element) async -> T
+    ) async -> [T] {
+        let tasks = map { element in
+            Task(priority: priority) {
+                await transform(element)
+            }
+        }
+        
+        return await tasks.asyncMap { task in
+            await task.value
+        }
+    }
+    
+    /// Transform the sequence into an array of new values using
+    /// an async closure.
+    ///
+    /// The closure calls will be performed concurrently, but the call
+    /// to this function won't return until all of the closure calls
+    /// have completed. If any of the closure calls throw an error,
+    /// then the first error will be rethrown once all closure calls have
+    /// completed.
+    ///
+    /// - parameter priority: Any specific `TaskPriority` to assign to
+    ///   the async tasks that will perform the closure calls. The
+    ///   default is `nil` (meaning that the system picks a priority).
+    /// - parameter transform: The transform to run on each element.
+    /// - returns: The transformed values as an array. The order of
+    ///   the transformed values will match the original sequence.
+    /// - throws: Rethrows any error thrown by the passed closure.
+    func concurrentMap<T>(
+        withPriority priority: TaskPriority? = nil,
+        _ transform: @escaping (Element) async throws -> T
+    ) async throws -> [T] {
+        let tasks = map { element in
+            Task(priority: priority) {
+                try await transform(element)
+            }
+        }
+        
+        return try await tasks.asyncMap { task in
+            try await task.value
+        }
+    }
+}
