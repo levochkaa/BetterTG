e3ca55af8cf6b0c36d476a1ccb7d0ed30a88113b
Lev Poznyakov <levochkaaa@icloud.com>
2023-01-27 16:24:36 +0300

    better logger

--- BetterTG/Utilities/Logger.swift
+++ BetterTG/Utilities/Logger.swift
@@ -3,22 +3,18 @@
 import Foundation
 import os.log
 
-// swiftlint:disable line_length
+// swiftlint:disable force_cast line_length
+
+let appName = Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as! String
+let projectName = Bundle.main.object(forInfoDictionaryKey: "CFBundleExecutable") as! String
+let logger = os.Logger(subsystem: appName, category: projectName)
+let dateFormatter = DateFormatter()
+
 func log(_ messages: Any..., file: String = #fileID, function: String = #function, line: Int = #line) {
-    // actually impossible cases
-    guard let appName = Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String else {
-        return print("Unable to get App Name")
-    }
-    guard let projectName = Bundle.main.object(forInfoDictionaryKey: "CFBundleExecutable") as? String else {
-        return print("Unable to get Project Name")
-    }
     guard let fileName = file.split(separator: "/").last?.split(separator: ".").first else {
         return print("Unable to get fileName info for \(file), with \(messages)")
     }
     
-    let logger = os.Logger(subsystem: appName, category: projectName)
-    
-    let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "HH:mm:ss"
     let date = dateFormatter.string(from: Date.now)
     
