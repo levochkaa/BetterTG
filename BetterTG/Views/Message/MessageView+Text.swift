b405fda5f2c10e4418c01b0bb66cb01d3d819663
levochkaa <levochkaaa@icloud.com>
2023-09-04 02:21:49 +0600

    redundant group

--- BetterTG/Views/Message/MessageView+Text.swift
+++ BetterTG/Views/Message/MessageView+Text.swift
@@ -4,23 +4,21 @@ import TDLibKit
 
 extension MessageView {
     @ViewBuilder var messageText: some View {
-        Group {
-            switch customMessage.message.content {
-                case .messageText(let messageText):
-                    formattedTextView(messageText.text)
-                case .messagePhoto(let messagePhoto):
-                    if !messagePhoto.caption.text.isEmpty {
-                        formattedTextView(messagePhoto.caption)
-                    }
-                case .messageVoiceNote(let messageVoiceNote):
-                    if !messageVoiceNote.caption.text.isEmpty {
-                        formattedTextView(messageVoiceNote.caption)
-                    }
-                case .messageUnsupported:
-                    Text("TDLib not supported")
-                default:
-                    Text("BTG not supported")
-            }
+        switch customMessage.message.content {
+            case .messageText(let messageText):
+                formattedTextView(messageText.text)
+            case .messagePhoto(let messagePhoto):
+                if !messagePhoto.caption.text.isEmpty {
+                    formattedTextView(messagePhoto.caption)
+                }
+            case .messageVoiceNote(let messageVoiceNote):
+                if !messageVoiceNote.caption.text.isEmpty {
+                    formattedTextView(messageVoiceNote.caption)
+                }
+            case .messageUnsupported:
+                Text("TDLib not supported")
+            default:
+                Text("BTG not supported")
         }
     }
     
