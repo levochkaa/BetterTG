93398c752235f9872c26e6af1f48a0cc2ee9e6f5
levochkaa <levochkaaa@icloud.com>
2023-12-16 21:46:59 +0300

    fix scrollToBottom button

--- BetterTG/ViewModels/ChatViewModel/ChatViewModel+Scroll.swift
+++ BetterTG/ViewModels/ChatViewModel/ChatViewModel+Scroll.swift
@@ -3,14 +3,6 @@
 import SwiftUI
 
 extension ChatViewModel {
-    func scrollToLast() {
-        guard let lastId = messages.last?.id, let scrollViewProxy else { return }
-        
-        withAnimation {
-            scrollViewProxy.scrollTo(lastId, anchor: .bottom)
-        }
-    }
-    
     func scrollTo(id: UUID?, anchor: UnitPoint = .center) {
         guard let scrollViewProxy, let id else { return }
         
--- BetterTG/Views/Chat/ChatView.swift
+++ BetterTG/Views/Chat/ChatView.swift
@@ -158,8 +158,45 @@ struct ChatView: View {
         )
     }
     
+    @ViewBuilder var scrollToBottomButton: some View {
+        Image(systemName: "chevron.down")
+            .offset(y: 1)
+            .font(.title3)
+            .padding(10)
+            .background(.black)
+            .clipShape(Circle())
+            .overlay {
+                Circle()
+                    .stroke(.blue, lineWidth: 1)
+            }
+            .overlay(alignment: .top) {
+                if viewModel.customChat.unreadCount != 0 {
+                    Circle()
+                        .fill(.blue)
+                        .frame(width: 16, height: 16)
+                        .overlay {
+                            Text("\(viewModel.customChat.unreadCount)")
+                                .font(.caption)
+                                .foregroundStyle(.white)
+                                .minimumScaleFactor(0.5)
+                        }
+                        .offset(y: -5)
+                }
+            }
+            .transition(.move(edge: .bottom).combined(with: .scale).combined(with: .opacity))
+            .padding(.trailing)
+            .padding(.bottom, 5)
+            .onTapGesture {
+                withAnimation {
+                    chatPosition = viewModel.messages.last?.id
+                }
+            }
+    }
+    
     func scrollToLastOnFocus() {
         guard scrollOnFocus else { return }
-        chatPosition = viewModel.messages.last?.id
+        withAnimation {
+            chatPosition = viewModel.messages.last?.id
+        }
     }
 }
