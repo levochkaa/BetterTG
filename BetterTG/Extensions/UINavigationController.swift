9bf29785e6c6a9ec003e87f09c09a82ab040d261
levochkaa <levochkaaa@icloud.com>
2024-04-21 17:09:38 +0300

    uiNavigationController (full swipe dismiss)

---
+++ BetterTG/Extensions/UINavigationController.swift
@@ -0,0 +1,21 @@
+// UINavigationController.swift
+
+import SwiftUI
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let panGesture = UIPanGestureRecognizer(
+            target: interactivePopGestureRecognizer?.delegate,
+            action: Selector(("handleNavigationTransition:"))
+        )
+        panGesture.delegate = self
+        view.addGestureRecognizer(panGesture)
+        interactivePopGestureRecognizer?.isEnabled = false
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        viewControllers.count > 1
+    }
+}
