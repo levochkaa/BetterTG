17f5d911f5b7f2b99341557c8ef8e6b11f47c66b
levochkaa <levochkaaa@icloud.com>
2025-01-12 19:47:58 +0400

    new navigation animations

--- BetterTG/Views/ChatsList/ChatsListView.swift
+++ BetterTG/Views/ChatsList/ChatsListView.swift
@@ -41,6 +41,7 @@ struct ChatsListView: View {
                         NavigationLink(value: customChat) {
                             ChatsListItemView(customChat: $customChat)
                                 .matchedGeometryEffect(id: customChat.chat.id, in: namespace)
+                                .matchedTransitionSource(id: customChat.chat.id, in: namespace)
                         }
                         .contextMenu {
                             contextMenu(for: customChat)
@@ -71,6 +72,7 @@ struct ChatsListView: View {
             .navigationTitle("BetterTG")
             .navigationDestination(for: CustomChat.self) { customChat in
                 ChatView(customChat: customChat)
+                    .navigationTransition(.zoom(sourceID: customChat.chat.id, in: namespace))
             }
             .confirmationDialog(
                 "Are you sure you want to delete chat with \(confirmedChat?.title ?? "User")?",
